9740afce5302d885a1c330301ec3473806f73835
RaubaAlexey <www.rauba@mail.ru>
2017-07-18 15:16:00 +0300

    commit #1 creating app / registration / logging in

---
+++ WebApplicationNew/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace WebApplicationNew.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
